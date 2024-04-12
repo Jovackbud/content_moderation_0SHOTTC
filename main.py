7256353dc28e1c3a0ad9eae99328ea0af7e7b4a1
Jovackbud <67655594+Jovackbud@users.noreply.github.com>
2024-04-12 13:38:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import transformers
+from tqdm import tqdm
+from transformers import pipeline
+import re
+
+text='get out shithead'
+
+
+labels = ['toxic', 'racist', 'gender bias', 'religious bias', 'aggressive', 
+          'personal attacks', 'hate speech', 'offensive language']
+
+
+# model chosen is fbbart
+model = pipeline("zero-shot-classification",
+                      model="facebook/bart-large-mnli")
+
+def zero_shot_classifier(text=text, model=model, labels=labels):
+    text = text
+    prediction = model(text, labels, multi_label=True)
+    if prediction["scores"][0] >= 0.75:
+        return True#, prediction["labels"][0]
+    
+
+# catch darkweb links
+def dark_web_links(text):
+    # Regular expression pattern to match common dark web link formats
+    dark_web_pattern = r"(https?://)?[a-z0-9]+\.onion(/[a-zA-Z0-9]*)*"
+
+    # Search for dark web links in the text
+    matches = re.findall(dark_web_pattern, text)
+
+    if matches:
+        return True
+    else:
+        return False
+    
+
+# catch adult content links
+def adult_content_sites(text):
+    # Regular expression pattern to match common adult content websites
+    adult_content_pattern = r"(https?://)?(?:www\.)?(pornhub\.com|xnxx\.com|youporn\.com|redtube\.com|etc)\b"
+
+    # Search for adult content sites in the text
+    matches = re.findall(adult_content_pattern, text)
+
+    # If any matches are found, flag them
+    if matches:
+        return True
+    else:
+        return False
+    
+
+def content_moderator(text, labels=labels, model=model, classifier=zero_shot_classifier, dkweb=dark_web_links, adult=adult_content_sites):
+    if dkweb(text):
+        return 'Darkweb link'
+    elif adult(text):
+        return 'Adult content site'
+    elif zero_shot_classifier(text):
+        return prediction["labels"][0]
+
+
+
+testing = content_moderator(text=text)
+print ("testing")
